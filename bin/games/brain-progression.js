071942304742ed01cb38d9e49a2104086ed912f1
Daria <daryaab321@gmail.com>
2023-05-09 17:29:42 +0300

    .

--- bin/games/brain-progression.js
+++ bin/games/brain-progression.js
@@ -13,16 +13,6 @@ const arithmeticProgress = (n, max) => {
 
   return newArray;
 };
-// Math.floor(
-
-// newArray[Math.floor(Math.random() * newArray.length)];
-// const arr = [1, 2, 3, 4, 5];
-// const getRandomArrayElement = (arr) => {
-//   let rand = Math.floor(Math.random()) * arr.length;
-//   const newElem = "..";
-//   arr.splice(rand, 1, "..");
-//   return [arr, rand];
-// };
 
 const readUserInput = () => {
   return readlineSync.question("Your answer: ");
@@ -32,7 +22,6 @@ const getQuestionParams = () => {
   let newArray = arithmeticProgress(1, 10);
   let rand = getRandomInt(0, newArray.length);
   const newElem = "..";
-  // let rightAnswer = newArray[rand].slice(rand, 1, newElem);
   let rightAnswer = newArray[rand];
   newArray[rand] = newElem;
   console.log("What number is missing in the progression?");
@@ -55,14 +44,3 @@ const verify = (readUserInput, rightAnswer) => {
 };
 
 startGame(getQuestionParams, readUserInput, verify);
-
-// console.log(newArray);
-
-// const getQuestionParams = (getRandomInt, newArray) => {
-//     let changeArr = newArray.splice()
-//   let firstNumber = getRandomInt(1, 50);
-//   let secondNumber = getRandomInt(1, 50);
-//   let rightAnswer = nod(firstNumber, secondNumber);
-//   console.log("Find the greatest common divisor of given numbers.");
-//   return [`Question: ${arithmeticProgress}`];
-// };
