577c4907de06a966bb3ff0083d21077957b4ea9a
Daria <daryaab321@gmail.com>
2023-06-12 18:25:44 +0300

    .

--- bin/games/brain-prime.js
+++ bin/games/brain-prime.js
@@ -15,45 +15,45 @@ const ariphMean = (...numbers) => {
 };
 
 ariphMean([10, 15]);
-// const readUserInput = () => {
-//   const choosing = ["yes", "no"];
-//   return readlineSync.question("Your answer: ", choosing);
-// };
+const readUserInput = () => {
+  const choosing = ["yes", "no"];
+  return readlineSync.question("Your answer: ", choosing);
+};
 
-// const isPrime = (num) => {
-//   for (let i = 2; i < num; i++) {
-//     if (num % i === 0) return false;
-//   }
-//   return num > 1;
-// };
-// // let num = 6;
-// // console.log(isPrime(num));
+const isPrime = (num) => {
+  for (let i = 2; i < num; i++) {
+    if (num % i === 0) return false;
+  }
+  return num > 1;
+};
+// let num = 6;
+// console.log(isPrime(num));
 
-// const getQuestionParams = (getRandomInt) => {
-//   let firstNumber = getRandomInt(1, 50);
-//   let rightAnswer = "";
-//   if (isPrime(firstNumber)) {
-//     rightAnswer = "yes";
-//   } else if (!isPrime(firstNumber)) {
-//     rightAnswer = "no";
-//   }
+const getQuestionParams = (getRandomInt) => {
+  let firstNumber = getRandomInt(1, 50);
+  let rightAnswer = "";
+  if (isPrime(firstNumber)) {
+    rightAnswer = "yes";
+  } else if (!isPrime(firstNumber)) {
+    rightAnswer = "no";
+  }
 
-//   console.log('Answer "yes" if given number is prime. Otherwise answer "no".');
-//   return [`Question: ${firstNumber}`, rightAnswer];
-// };
+  console.log('Answer "yes" if given number is prime. Otherwise answer "no".');
+  return [`Question: ${firstNumber}`, rightAnswer];
+};
 
-// const verify = (readUserInput, rightAnswer) => {
-//   if (readUserInput == rightAnswer) {
-//     return [true, "Correct!"];
-//   } else {
-//     if (readUserInput == "yes" && rightAnswer == "no") {
-//       return [false, "'yes' is wrong answer ;(. Correct answer was 'no'."];
-//     } else if (readUserInput == "no" && rightAnswer == "yes") {
-//       return [false, "'no' is wrong answer ;(. Correct answer was 'yes'.!"];
-//     } else {
-//       return [false, ""];
-//     }
-//   }
-// };
+const verify = (readUserInput, rightAnswer) => {
+  if (readUserInput == rightAnswer) {
+    return [true, "Correct!"];
+  } else {
+    if (readUserInput == "yes" && rightAnswer == "no") {
+      return [false, "'yes' is wrong answer ;(. Correct answer was 'no'."];
+    } else if (readUserInput == "no" && rightAnswer == "yes") {
+      return [false, "'no' is wrong answer ;(. Correct answer was 'yes'.!"];
+    } else {
+      return [false, ""];
+    }
+  }
+};
 
-// startGame(getQuestionParams, readUserInput, verify);
+startGame(getQuestionParams, readUserInput, verify);
