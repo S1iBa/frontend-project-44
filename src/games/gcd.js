c8d321e21b7847a4dca6eb3dc1e9461c1d3d0c61
Daria <daryaab321@gmail.com>
2024-07-02 22:26:35 +0300

    some fixes 4(1)

--- src/games/gcd.js
+++ src/games/gcd.js
@@ -4,11 +4,7 @@ import getRandomInt from '../utils.js';
 
 const gameIntro = 'Find the greatest common divisor of given numbers.';
 
-const nod = (num1, num2) => {
-  if (num2 > num1) return nod(num2, num1);
-  if (!num2) return num1;
-  return nod(num2, num1 % num2);
-};
+const nod = (num1, num2) => (num2 === 0 ? num1 : nod(num2, num1 % num2));
 
 const gameGeneration = () => {
   const firstNumber = getRandomInt(1, 50);
